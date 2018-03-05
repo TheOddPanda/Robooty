1ffbd59ec1dbf42b47bd10db0d6b042e8f276ca6
TheOddPanda <adrianbuechi@gmail.com>
2018-03-05 11:20:36 +0100

    add base

---
+++ src/DriverTask.java
@@ -0,0 +1,10 @@
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+
+//I AM THE DRIVAH
+public class DriverTask {
+    private EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
+    private EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.D);
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
 public class Main {
-    public static void main(String[] args) {
-        Sound.buzz();
+    public static void main(String[] args) throws InterruptedException {
+        EV3LargeRegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.A);
+        motor.setSpeed(200);
+        motor.forward();
+        Thread.sleep(1000);
     }
 }
