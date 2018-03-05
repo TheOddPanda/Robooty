85c90c99e94eda966a08317ad9fda415e018bfe3
TheOddPanda <adrianbuechi@gmail.com>
2018-03-05 11:26:42 +0100

    added to very useful methods

--- src/DriverTask.java
+++ src/DriverTask.java
@@ -2,9 +2,23 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
 //I AM THE DRIVAH
-public class DriverTask {
+public class DriverTask implements Runnable {
     private EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
     private EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.D);
 
+    @Override
+    public void run() {
+
+    }
+
+    private void driveForward() {
+        motorL.forward();
+        motorR.forward();
+    }
+
+    private void driveBackwards() {
+        motorL.backward();
+        motorR.backward();
+    }
 
 }
